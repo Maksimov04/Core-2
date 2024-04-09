d53259987d70b5f1df579548c198babbc3381c38
Илья <pandas1987bag@mail.ru>
2024-04-09 22:12:58 +0300

    Решение задачи

--- src/task/collection/Collection.java
+++ src/task/collection/Collection.java
@@ -135,6 +135,31 @@ import java.util.HashSet;
  }
 
      // TODO: Удалить все элементы из множества, которые также содержатся в другом множестве.
+      class sss {
+         public static void main(String args[]) {
+             HashSet<Integer> set1 = new HashSet<>();
+             set1.add(1);
+             set1.add(3);
+             set1.add(4);
+             set1.add(5);
+             set1.add(2);
+             set1.add(6);
+
+
+             HashSet<Integer> set2 = new HashSet<>();
+             set2.add(1);
+             set2.add(2);
+             set2.add(3);
+             set2.add(4);
+             set2.add(5);
+             set2.add(6);
+
+             HashSet<Integer> deletion = new  HashSet(set1);
+             deletion.removeAll(set2);
+             System.out.println(deletion);
+         }
+
+     }
 
     // TODO: Проверить, содержит ли карта определенный ключ.
 
